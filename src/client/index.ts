6981b66994fa6bfe48231520595760061784dab9
Derrick Yoo <derrickyoo@gmail.com>
2026-03-05 08:18:32 -0800

    update client with command

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,14 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  commandStatus,
+  getInput,
+  printClientHelp,
+  printQuit,
+} from "../internal/gamelogic/gamelogic.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandMove } from "../internal/gamelogic/move.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
@@ -32,7 +41,38 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient,
   );
+
+  const gs = new GameState(username);
+
+  while (true) {
+    const words = await getInput();
+    if (words.length === 0) continue;
+
+    const command = words[0];
+    if (command === "move") {
+      try {
+        commandMove(gs, words);
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "status") {
+      commandStatus(gs);
+    } else if (command === "spawn") {
+      commandSpawn(gs, words);
+    } else if (command === "help") {
+      printClientHelp();
+    } else if (command === "quit") {
+      printQuit();
+      process.exit(0);
+    } else if (command === "spam") {
+      console.log("Spamming not allowed yet!");
+    } else {
+      console.log("Unknown command");
+      continue;
+    }
+  }
 }
+
 main().catch((err) => {
   console.error("Fatal error:", err);
   process.exit(1);
