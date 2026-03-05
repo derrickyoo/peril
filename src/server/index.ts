54dc15ef99cb77ea5303e1206d17ffe517f78853
Derrick Yoo <derrickyoo@gmail.com>
2026-03-04 18:00:07 -0800

    publish JSON

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
 import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
 async function main() {
-  console.log("Starting Peril server...");
-
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   console.log("Peril game server connected to RabbitMQ!");
@@ -12,13 +12,23 @@ async function main() {
       try {
         await conn.close();
         console.log("RabbitMQ connection closed.");
-      } catch (err){
-        console.error('Error closing RabbitMQ connection:', err);
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
       } finally {
         process.exit(0);
       }
-    })
-  )
+    }),
+  );
+
+  const publishCh = await conn.createConfirmChannel();
+
+  try {
+    await publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
+      isPaused: true,
+    });
+  } catch (err) {
+    console.error("Error publishing message:", err);
+  }
 }
 
 main().catch((err) => {
